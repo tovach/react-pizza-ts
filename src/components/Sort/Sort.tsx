b1fe8e164bd3b5e8e69b8339bad027e50a3a62f9
tovach <red5.dalv@gmail.com>
2022-01-19 16:45:30 +0300

    feat: add dispatch on mount

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,13 +1,12 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {ReactComponent as SortIcon} from "./img/sort-icon.svg";
 
 import styles from './Sort.module.scss';
+import {useAppDispatch} from "../../hooks/redux";
+import {setSort} from "../../store/slices/goodsSlice";
+import {SortOption} from "../../types/interfaces";
 
-interface SortOption {
-    name: string,
-    value: string,
-    order: string,
-}
+
 
 const Sort: FC = () => {
 
@@ -20,13 +19,17 @@ const Sort: FC = () => {
     const [visible, setVisible] = useState(false);
     const [current, setCurrent] = useState<SortOption>(sortVariants[0])
 
+    const dispatch = useAppDispatch();
+
 
     const onChoice = (el: SortOption) => {
         setVisible(!visible);
         setCurrent(el);
     }
 
-//TODO: Here useEffect with axios deps[current]
+    useEffect(() => {
+        dispatch(setSort(current));
+    }, [current])
 
     return (
         <div className={styles.wrapper}>
