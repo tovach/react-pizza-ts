d52bc47e690ab5f35d455f991e12d5582215ed2c
tovach <red5.dalv@gmail.com>
2022-01-20 20:29:41 +0300

    feat: add CartItem component

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,50 @@
+import React, {FC} from 'react';
+import {PizzaItemCart} from "../../types/interfaces";
+
+import {ReactComponent as PlusIcon} from "./img/plus-icon.svg";
+import {ReactComponent as MinusIcon} from "./img/minus-icon.svg";
+import {ReactComponent as RemoveIcon} from "./img/remove-icon.svg";
+
+import styles from './CartItem.module.scss';
+
+interface CartItemProps {
+    item: PizzaItemCart
+}
+
+const CartItem: FC<CartItemProps> = ({item}) => {
+    return (
+        <article className={styles.wrapper}>
+
+            <div className={styles.left}>
+                <div className={styles.image}>
+                    <img src={item.img} alt={item.title}/>
+                </div>
+
+                <div className={styles.text}>
+                    <h4>{item.title}</h4>
+                    <p>{item.dough}, {item.size}</p>
+                </div>
+            </div>
+
+            <div className={styles.right}>
+
+                <div className={styles.controls}>
+                    <button className={styles.button}><PlusIcon/></button>
+                    <span>{item.quantity}</span>
+                    <button className={styles.button}><MinusIcon/></button>
+                </div>
+
+                <div className={styles.price}>
+                    <h3>{item.quantity * item.price}</h3>
+                </div>
+
+                <div className={styles.remove}>
+                    <button className={styles.button}><RemoveIcon/></button>
+                </div>
+            </div>
+
+        </article>
+    );
+};
+
+export default CartItem;
