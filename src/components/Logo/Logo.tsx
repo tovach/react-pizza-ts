4ca7a2925a86e93292c32911057442a2a678cf3b
tovach <red5.dalv@gmail.com>
2022-01-20 20:31:02 +0300

    feat: add routes

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,17 +1,19 @@
 import React, {FC} from 'react';
-import { ReactComponent as LogoIcon } from './img/logo-icon.svg';
-
+import {ReactComponent as LogoIcon} from './img/logo-icon.svg';
+import {Link} from "react-router-dom";
 import styles from './Logo.module.scss';
 
-const Logo:FC = () => {
+const Logo: FC = () => {
     return (
-        <div className={styles.wrapper}>
-            <LogoIcon/>
-            <div>
-                <h1 className={styles.heading}>REACT PIZZA</h1>
-                <h5 className={styles.subHeading}>самая вкусная пицца во вселенной</h5>
+        <Link to={'/'}>
+            <div className={styles.wrapper}>
+                <LogoIcon/>
+                <div>
+                    <h1 className={styles.heading}>REACT PIZZA</h1>
+                    <h5 className={styles.subHeading}>самая вкусная пицца во вселенной</h5>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
--- src/components/OrderCounter/OrderCounter.tsx
+++ src/components/OrderCounter/OrderCounter.tsx
@@ -1,10 +1,12 @@
 import React, {FC} from 'react';
 import {ReactComponent as CartIcon} from "./img/cart-icon.svg";
+import {Link} from "react-router-dom";
 
 import styles from './OrderCounter.module.scss';
 
 const OrderCounter:FC = () => {
     return (
+        <Link to={'/cart'}>
             <button className={styles.wrapper}>
                 <span className={styles.price}>0 ₽</span>
                 <span className={styles.divider}></span>
@@ -12,6 +14,7 @@ const OrderCounter:FC = () => {
                     <CartIcon/>
                     0</span>
             </button>
+        </Link>
     );
 };
 
